4e9241e2571fcecadcef509c1ea2f8754ec56638
laymanmu <laymanmu@FOREST>
2020-02-09 19:03:51 -0800

    added a test

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -2,18 +2,31 @@ package com.example.springboot;
 
 import java.util.Arrays;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(Application.class, args);
+	}
 
+
+	@Bean
+	public CommandLineRunner startup(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("before");
+			inspectBeans(ctx);
+			System.out.println("after");
+		};
+	}
+
+	private static void inspectBeans(ApplicationContext ctx) {
 		System.out.println("Let's inspect the beans provided by Spring Boot:");
-
 		String[] beanNames = ctx.getBeanDefinitionNames();
 		Arrays.sort(beanNames);
 		for (String beanName : beanNames) {
