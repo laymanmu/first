068fa063015283681511ba27589c70f6218a7633
laymanmu <laymanmu@FOREST>
2020-02-12 07:02:53 -0800

    examples for redis, kafka

---
+++ scripts/start_redis.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker run -it --rm -p 6379:6379 -v C:\\data\\redis://data redis
+
--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -2,6 +2,9 @@ package com.example.springboot;
 
 import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,27 +13,41 @@ import java.util.List;
 
 @RestController
 public class HelloController {
+	private static Gson gson = new Gson();
 
 	@Autowired
 	UserRepository userRepository;
 
+	@Autowired
+	KafkaTemplate<String, String> kafka;
+
+	@Value("${kafka.topic}")
+	private String kafkaTopic;
+
 	@RequestMapping("/")
 	public String index() {
-		User newUser = new User.Builder()
-				.withName("user1")
-				.withExperience(1)
-				.withLevel(User.Level.Apprentice)
-				.build();
-		userRepository.save(newUser);
+		return buildUserList();
+	}
 
+	@RequestMapping(method=RequestMethod.POST, value="/user")
+	public String newUser(User user) {
+		userRepository.save(user);
+		String userJson = gson.toJson(user);
+		kafka.send(kafkaTopic, userJson);
+		return userJson;
+	}
+
+	private String buildUserList() {
 		List<User> users = new ArrayList<>();
 		userRepository.findAll().forEach(users::add);
 
-		Gson gson = new Gson();
 		StringBuilder sb = new StringBuilder();
+		sb.append("<ol>\n");
 		for (User user : users) {
-			sb.append(gson.toJson(user)).append("<br>\n");
+			String userJson = gson.toJson(user);
+			sb.append("<li>").append(userJson).append("</li>\n");
 		}
+		sb.append("</ol>\n");
 		return sb.toString();
 	}
 }
